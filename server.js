4eb11a1c7c2e911eb7b0fa601937ae2d1313da21
Aareen80085 <aareend@gmail.com>
2026-09-21 13:38:39 +0530

    server update

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 
 app.use(express.json());
 
-app.post('/sendData',(req,res)=>{
+app.post('/sendData', (req, res) => {
     const data = req.body;
-    console.log(data)
-res.send('Post request received')
-})
-app.get('/getData',(req,res)=>{
-    res.send('Get request received')
-    res.json(data)
-})
 
+    fs.writeFileSync('data.json',JSON.stringify(data, null, 2));
 
-app.listen(2000,()=>{
-    console.log('The server is running at local host 2000')
-})
+    console.log(data);
+
+    res.send('Post request received');
+});
+
+app.get('/getData', (req, res) => {
+    const data = fs.readFileSync('data.json', 'utf-8');
+
+    res.json(JSON.parse(data));
+});
+
+app.listen(process.env.PORT || 2000, () => {
+    console.log('The server is running at local host 2000');
+});
